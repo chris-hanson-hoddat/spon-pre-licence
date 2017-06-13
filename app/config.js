593878cd9a6624024f0381b7097777d23863d24a
Chris Hanson <chris.hanson@digital.homeoffice.gov.uk>
2017-06-13 12:22:52 +0100

    Set up app name Added updated styles for check answers Copied across some starter pages Added a categories page for SP-2182

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Get permission for people to work in the UK',
 
   // Default port that prototype runs on
   port: '3000',
